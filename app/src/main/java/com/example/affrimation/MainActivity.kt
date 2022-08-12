d85f5485826e307f59cc8d5278f66088765bb8d3
taufik0910 <taufik.cradle@gmail.com>
2022-08-13 04:05:21 +0700

    initial recyclerview pada main activity

--- app/src/main/java/com/example/affrimation/MainActivity.kt
+++ app/src/main/java/com/example/affrimation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.affrimation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affrimation.adapter.ItemAdapter
 import com.example.affrimation.data.Datasource
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 //        val textView : TextView =findViewById(R.id.txt_hello_world)
 //        textView.text = Datasource().loadAffirmations().size.toString()
+//            initialize data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter =  ItemAdapter(this,myDataset)
+        recyclerView.setHasFixedSize(true)
+
     }
 }
